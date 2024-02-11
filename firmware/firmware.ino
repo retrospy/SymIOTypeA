6dcf0b2e3644bef178e8409e4cd300143bcc5641
Christopher J Mallery <zoggins@retro-spy.com>
2024-02-11 11:53:01 -0800

    Added support for raspberry pi pico

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,6 +1,11 @@
+#if !defined(ARDUINO_RASPBERRY_PI_PICO)
 #include "SlowSoftSerial.h"
 
 SlowSoftSerial sss(0,1);
+#else
+#define sss Serial1
+#endif
+
 char currentCommand;
 bool awaitingCommand = false;
 bool awaitingSecondaryCommand = false;
@@ -10,17 +15,27 @@ const char* buadRatesStr[6]= {"110", "300", "600", "1200", "2400", "4800"};
 const char* buadRatesCode[6]= {"D5", "4C", "24", "10", "06", "01"};
 const bool buadRatesEcho[6]= {true, true, false, false, false, false};
 
+ #define AIRCR_Register (*((volatile uint32_t*)(PPB_BASE + 0x0ED0C)))
+
 void setup() {
   pinMode(2, INPUT_PULLUP);
   Serial.begin(115200);
+#if defined(ARDUINO_RASPBERRY_PI_PICO)
+  Serial1.setRX(1);
+  Serial1.setTX(0);
+#endif
   sss.begin(buadRates[baudIdx]);
 }
 
 void loop() {
 
   if (digitalRead(2) == LOW)
-   _reboot_Teensyduino_();
-    
+#if !defined(ARDUINO_RASPBERRY_PI_PICO)
+    _reboot_Teensyduino_();
+#else
+    AIRCR_Register = 0x5FA0004;
+#endif
+
   if (Serial.available()) 
   {      
     char c = Serial.read();
